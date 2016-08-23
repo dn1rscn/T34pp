04a88d77b6c1b227aab2617c0cdb6fff341b73ad
JPalacio3D <jorge.jp92@gmail.com>
2016-08-23 21:11:41 +0200

    Actualizacion
    subido dado, sonidos, emociones y personalizacion. posiblemente falten
    cosas

---
+++ Assets/Scripts/Personalizacion/Actualizar_ninos.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actualizar_ninos : MonoBehaviour 
+{
+	public GameObject[] AGeo_complementos;
+	public GameObject[] AGeo_Pelo;
+	public GameObject[] AGeo_Piernas;
+	
+	public Texture[] Atexture_Piel;
+	public Texture[] Atexture_camiseta;
+
+	control_datosGlobalesPersonalizacion cdgp;
+
+	// Use this for initialization
+	void Start () 
+	{
+		/*cdgp = GameObject.Find ("datosGlobalesPersonalizacion").GetComponent<control_datosGlobalesPersonalizacion> ();
+
+		AGeo_complementos [cdgp.posicion_complementos].SetActive (true);
+		AGeo_Pelo [cdgp.posicion_pelo].SetActive (true);
+		GameObject.Find("Piel").GetComponent<Renderer>().material.mainTexture=Atexture_Piel[cdgp.posicion_piel];
+		GameObject.Find("Sudadera").GetComponent<Renderer>().material.mainTexture=Atexture_camiseta[cdgp.posicion_camiseta];
+		AGeo_Piernas [cdgp.posicion_piernas].SetActive (true);
+		if (cdgp.posicion_complementos != 0) 
+		{
+			AGeo_complementos [cdgp.posicion_complementos].GetComponent<Renderer> ().material.mainTexture = cdgp.Atexture_camiseta [cdgp.posicion_camiseta];
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/Personalizacion/controlRA_personajes_mascotas.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlRA_personajes_mascotas : MonoBehaviour 
+{
+	control_datosGlobalesPersonalizacion cdgp;
+
+	public GameObject mascotas;
+	public GameObject avatar;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cdgp = GameObject.Find ("datosGlobalesPersonalizacion").GetComponent<control_datosGlobalesPersonalizacion> ();
+
+		if (cdgp.mascotas == true) 
+		{
+			mascotas.SetActive (true);
+			avatar.SetActive(false);
+		} 
+		else 
+		{
+			mascotas.SetActive (false);
+			avatar.SetActive(true);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
